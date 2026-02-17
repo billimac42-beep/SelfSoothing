74cb0c32c72b1bf5f8faac726153bea26102bdb2
billimac42-beep <billimac42@gmail.com>
2026-02-17 14:54:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function() {
         loader.remove();
       }, 1500); 
       
-    }, 6000); 
+    }, 4000); 
   } else if (loader) {
     loader.style.display = 'none';
   }
